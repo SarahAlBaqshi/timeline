834e0f061068f9dc8c1a2ddad5e95cf395c8f5fd
Sarah <S00038939@auk.edu.kw>
2020-07-01 17:29:21 +0300

    minor code changes to timeline project

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { TimelineItemStyle, TimelineContainer2 } from "../styles";
+import TimelineItemStyle from "../styles";
 
 const TimelineItem = ({ data }) => (
-  //   <div className="timeline-item">
   <TimelineItemStyle>
     <p>{data.date}</p>
     <p>{data.text}</p>
   </TimelineItemStyle>
-  //   {/* </div> */}
 );
 
 export default TimelineItem;
